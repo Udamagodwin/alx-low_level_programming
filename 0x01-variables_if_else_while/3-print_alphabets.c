bd9d87a4f57bde9bd9f958302a1ebdb7bf2d83ac
Udamagodwin <udamagodwin@gmail.com>
2023-02-19 18:37:33 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * more headers goes there
+ * main - A program that print alphabet lower and upper case
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
