f7753b9b70d9db3da41c8c9e4313f2f21161a630
Udamagodwin <udamagodwin@gmail.com>
2023-02-20 10:49:01 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
 /**
  * main: A program that print a line to the standard eror
  * Return: 1 (success)
  */
 int main(void)
 {
-puts("and that piece of art is useful"\"- Dora Korpar, 2015-10-19\n");
+puts("and that piece of art is useful""- Dora Korpar, 2015-10-19\n");
 return (1);
 }
 
