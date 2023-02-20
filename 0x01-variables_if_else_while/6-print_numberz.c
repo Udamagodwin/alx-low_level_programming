f01b31d77f7ff148162cd30148ed376fec744afc
Udamagodwin <udamagodwin@gmail.com>
2023-02-19 19:49:26 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * more headers goes there
+ * main - A program that print alphabet alphabet digit
+ * numbers in base 10 starting from
+ * 0. followed by a new line
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
