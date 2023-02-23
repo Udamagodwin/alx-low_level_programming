20c7d0a6433e7343c4383a80d3eedcfa4473eb3d
Udamagodwin <udamagodwin@gmail.com>
2023-02-23 01:58:10 -0800

    main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+2-largest_number.c(int a,int b, int c);
+remove comment in(int comment)
+print_remaining_days( int month, int day, int year)
+;
+#endif /* MAIN_H */
