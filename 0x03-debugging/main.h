f63e4352b182c440949a542f0b3db19b6bfb88fe
Udamagodwin <udamagodwin@gmail.com>
2023-02-23 03:05:57 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
-2-largest_number.c(int a,int b, int c);
-remove comment in(int comment)
-print_remaining_days( int month, int day, int year)
+int largest_number(int a, int b, int c);
+void dele_lapses(int l, int b);
+void print_remaining_days(int month, int day, int year)
 ;
 #endif /* MAIN_H */
