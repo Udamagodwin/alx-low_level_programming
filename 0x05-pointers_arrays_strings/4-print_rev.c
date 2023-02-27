6812dd4ce989ed92f8e86ad16a77d4570fa29de2
Udamagodwin <udamagodwin@gmail.com>
2023-02-27 14:19:14 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_puts - print a strings in reverse form
+*@a: string to be printed
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
