cf75e5971c6e2a47e5a6205574577708e482514a
Udamagodwin <udamagodwin@gmail.com>
2023-02-27 14:44:06 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
-*_puts - print a strings in reverse form
+*print_rev - print a strings in reverse form
 *@s: pointer to the string to be printed
+Return: void
 */
 void print_rev(char *s)
 {
 	int i = 0;
 
 	while (s[i])
+
 	{
 		i++;
 		while (i--)
