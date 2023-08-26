fc64ddd110c94da7d5dbe6ede32a9b69474a7b5c
Udama Godwin <udamagodwin@gmail.com>
2023-08-25 20:11:06 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strlen - Return the length of strings
+*@a: strings
+*return: length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+	{
+		len++;
+	}
+
+	return (len);
+
+}
